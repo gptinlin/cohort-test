8d684de2bb2eba7f623ab1532c14fcc275e991fc
gptinlin <garrett.tinlin@gmail.com>
2020-10-10 11:41:27 -0700

    djfgkhser5

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-var i = 0;
-
-function timedCount() {
-    i = i + 1;
-    postMessage(i);
-    setTimeout("timedCount()",500);
+let slideInterval = 3500;
+function getFigures() {
+    return document.getElementById('carousel').getElementsByTagName('figure');
 }
-
-timedCount();
-
-let worker;
-function startWorker() {
-    worker = new Worker("worker.js");
-    worker.onmessage = function(event) {
-        document.getElementById("output").innerHTML += '<li>' + event.data + '</li>';
-    };
+function moveForward() {
+    var pointer;
+    var figures = getFigures();
+    for (var i = 0; i < figures.length; i++) {
+        if (figures[i].className == 'visible') {
+            figures[i].className = '';
+            pointer = i;
+        }
+    }
+    if (++pointer == figures.length) {
+        pointer = 0;
+    }
+    figures[pointer].className = 'visible';
+    setTimeout(moveForward, slideInterval);
 }
-function stopWorker() {
-    worker.terminate();
+function startPlayback() {
+    setTimeout(moveForward, slideInterval);
 }
+startPlayback();
